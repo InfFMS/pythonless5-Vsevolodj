eaea71f951ad8a09d249115c12ae4ea82fcd12c9
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2024-11-02 14:41:20 +0700

    Сделал задачу 6

--- task6.py
+++ task6.py
@@ -4,3 +4,16 @@
 # Пример: ввод N = 6
 # [1,2,3,4,5,6]
 # Вывод: [3,2,1,6,5,4]
+from random import randint
+def razvor(list, len):
+    a=[]
+    for i in range(-1, -1*len-1, -1):
+        a.append(list[i])
+    return a
+n=int(input())
+lst=[randint(1, 10) for i in range(n)]
+print(lst)
+l=lst[:n//2]
+r=lst[n//2:]
+s = razvor(l, n//2) + razvor(r, n//2)
+print(s)
